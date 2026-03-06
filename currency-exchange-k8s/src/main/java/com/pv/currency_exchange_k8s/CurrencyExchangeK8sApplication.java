620671940641018df7d9b2ff56efb229bf481512
ParameshwariV27 <parameshwaribv27@gmail.com>
2026-03-06 21:28:52 +0530

    Update CurrencyExchangeK8sApplication.java

--- currency-exchange-k8s/src/main/java/com/pv/currency_exchange_k8s/CurrencyExchangeK8sApplication.java
+++ currency-exchange-k8s/src/main/java/com/pv/currency_exchange_k8s/CurrencyExchangeK8sApplication.java
@@ -3,6 +3,7 @@ package com.pv.currency_exchange_k8s;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CurrencyExchangeK8sApplication {
 
